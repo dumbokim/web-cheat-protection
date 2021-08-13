808253656f18af53fea9b153e72da02a42ef6cc8
DumboKim <rlawlsgus97@gmail.com>
2021-08-13 22:38:49 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
+
+const CONSTRAINTS = { video: true };
 
 function App() {
+  const videoRef = useRef(null);
+
+  const startVideo = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia(CONSTRAINTS);
+    if (videoRef && videoRef.current && !videoRef.current.srcObject) {
+      videoRef.current.srcObject = stream;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay ref={videoRef} />
+      <button onClick={startVideo}>start</button>
     </div>
   );
 }
